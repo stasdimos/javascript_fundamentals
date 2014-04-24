e08c32d54333b2d70c9b09eb07db1cc0f40a7939
stasdimos <stas.dimos@icloud.com>
2014-04-24 14:25:15 -0400

    Finished arrays

--- arrays.js
+++ arrays.js
@@ -6,5 +6,17 @@
 // For each choice, log to the screen a string like: "My #1 choice is blue."
 // Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
 
+var topChoices = ["Blue", "Red", "Yellow"];
 
+for ( var i = 0; i < topChoices.length; i++){
+  if ( i === 0 ){
+    console.log("My " + (i+1) + "st choice is " + topChoices[i]);
+  } else if ( i === 1 ) {
+    console.log("My " + (i+1) + "nd choice is " + topChoices[i]);
+  } else if ( i === 2 ) {
+    console.log("My " + (i+1) + "rd choice is " + topChoices[i]);
+  } else {
+    console.log("Error");
+  }
+}
 
--- functions.js
+++ functions.js
@@ -121,3 +121,4 @@ fahrenheitToCelsius(70)
 
 
 
+
